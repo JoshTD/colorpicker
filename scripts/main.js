bb20e27fa50d51d841b299bd931d2a8656e35c33
Josh Khrustalev <khrustalevjosh@gmail.com>
2022-07-28 12:28:31 +0300

    LocalStorage test

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,60 @@
 var colors = [
-    {name:'Мятное утро', type:'base', color:'#86EAE9'},
-    {name:'Лавандовый', type:'main', color:'#C79FEF'},
-    {name:'Спокойный сине-голубой', type:'main', color:'#37C9EF'},
+  { name: 'Мятное утро', type: 'base', color: '#86EAE9' },
+  { name: 'Лавандовый', type: 'main', color: '#C79FEF' },
+  { name: 'Спокойный сине-голубой', type: 'main', color: '#37C9EF' },
 ];
+
+let storage = window.localStorage;
+
+for (let i = 0; i < colors.length; i++) {
+  storage.setItem('color' + i, JSON.stringify(colors[i]));
+}
+
+console.log(storage);
+
+for (let key of Object.keys(storage)) {
+  let item = storage[key];
+  console.log(JSON.parse(item));
+}
 
 let table = document.getElementById('table');
 
-colors.forEach(color => {
-    table.appendChild(generateRow(color));
-})
+colors.forEach((color) => {
+  table.appendChild(generateRow(color));
+});
 
 function generateRow(color) {
-    let row = document.createElement('div');
-    row.className = 'table-row';
+  let row = document.createElement('div');
+  row.className = 'table-row';
 
-    let cells = [];
-    cells.push(generateCellWithIcon('color-square', 'color', color.color));
+  let cells = [];
+  cells.push(generateCellWithIcon('color-square', 'color', color.color));
 
-    for (let key in color) {
-        cells.push(generateCell(color[key]));
-    }
+  for (let key in color) {
+    cells.push(generateCell(color[key]));
+  }
 
-    cells.push(generateCellWithIcon('color-change-icon',  'color-change'));
-    cells.push(generateCellWithIcon('color-remove-icon', 'color-remove'));
+  cells.push(generateCellWithIcon('color-change-icon', 'color-change'));
+  cells.push(generateCellWithIcon('color-remove-icon', 'color-remove'));
 
-    cells.forEach(cell => row.appendChild(cell));
-    
-    return row;
+  cells.forEach((cell) => row.appendChild(cell));
+
+  return row;
 }
 
 function generateCell(text) {
-    let cell = document.createElement('div');
-    cell.className = 'table-item'
-    cell.innerText = text ?? '';
-    return cell;
+  let cell = document.createElement('div');
+  cell.className = 'table-item';
+  cell.innerText = text ?? '';
+  return cell;
 }
 
 function generateCellWithIcon(iconName, className, color) {
-    let cell = generateCell();
-    cell.className += className ? ' ' + className : '';
-    let icon = document.createElement('div');
-    icon.className = iconName;
-    icon.style.backgroundColor = color ?? '';
-    cell.appendChild(icon);
-    return cell;
+  let cell = generateCell();
+  cell.className += className ? ' ' + className : '';
+  let icon = document.createElement('div');
+  icon.className = iconName;
+  icon.style.backgroundColor = color ?? '';
+  cell.appendChild(icon);
+  return cell;
 }
